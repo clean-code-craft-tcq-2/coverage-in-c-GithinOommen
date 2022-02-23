0256074496288efbcdc9d9c83f90d4234de9b58d
GithinOommen <90901501+GithinOommen@users.noreply.github.com>
2022-02-23 21:03:16 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -6,3 +6,6 @@
 TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
 }
+TEST_CASE("infers the breach according to limits") {
+  REQUIRE(inferBreach(50, 20, 30) == TOO_HIGH);
+}
