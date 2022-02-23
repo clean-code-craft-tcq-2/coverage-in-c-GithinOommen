de780c8d62eb181254b6b62e6d3c6b04580fe56a
GithinOommen <90901501+GithinOommen@users.noreply.github.com>
2022-02-24 00:28:31 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -22,24 +22,7 @@ BreachType inferBreach(double value, double lowerLimit, double upperLimit) {
 
 BreachType classifyTemperatureBreach(
     CoolingType coolingType, double temperatureInC) {
-            /*
-  int lowerLimit = 0;
-  int upperLimit = 0;
-  switch(coolingType) {
-    case PASSIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 35;
-      break;
-    case HI_ACTIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 45;
-      break;
-    case MED_ACTIVE_COOLING:
-      lowerLimit = 0;
-      upperLimit = 40;
-      break;
-  }
-  */
+
   return inferBreach(temperatureInC, BreachArr[coolingType].lowerLimit, BreachArr[coolingType].upperLimit);
 }
 
@@ -48,38 +31,19 @@ SendAlert_to checkAndAlert(
 
   BreachType breachType = classifyTemperatureBreach( batteryChar.coolingType, temperatureInC);
 
- /* switch(alertTarget) {
-    case TO_CONTROLLER:
-      sendToController(breachType);
-      break;
-    case TO_EMAIL:
-      sendToEmail(breachType);
-      break;
-  }
- */
 SendAlert_to checkAndAlertWrapper = (*AlertTargetFunc[alertTarget])(breachType);
+	return checkAndAlertWrapper;
 }
 
 SendAlert_to sendToController(BreachType breachType) {
   const unsigned short header = 0xfeed;
   printf("%x : %x\n", header, breachType);
+	return TO_CONTROLLER;
 }
 
 SendAlert_to sendToEmail(BreachType breachType) {
   const char* recepient = "a.b@c.com";
- /* switch(breachType) {
-    case TOO_LOW:
-      printf("To: %s\n", recepient);
-      printf("Hi, the temperature is too low\n");
-      break;
-    case TOO_HIGH:
-      printf("To: %s\n", recepient);
-      printf("Hi, the temperature is too high\n");
-      break;
-    case NORMAL:
-      break;
-  }
- */  SendAlert_to MailSendStatus= (*SendmailAS[breachType])(recepient);
+ SendAlert_to MailSendStatus= (*SendmailAS[breachType])(recepient);
 	  return MailSendStatus;
 }
 
